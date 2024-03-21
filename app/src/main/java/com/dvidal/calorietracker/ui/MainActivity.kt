584ddc95a7b841b0cf11255094546a3a4768442d
Diego Vidal <diego.vidal@home24.de>
2024-03-21 20:14:47 +0100

    welcome screen and utility

--- app/src/main/java/com/dvidal/calorietracker/ui/MainActivity.kt
+++ app/src/main/java/com/dvidal/calorietracker/ui/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.dvidal.calorietracker.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.dvidal.calorietracker.ui.theme.CalorieTrackerTheme
+import com.dvidal.onboarding_presentation.welcome.WelcomeScreen
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalorieTrackerTheme {
+                WelcomeScreen()
+            }
+        }
+    }
 }
