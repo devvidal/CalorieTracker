22d83c20fae5965a143487dc408d197293d00f95
Diego Vidal <diego.vidal@home24.de>
2024-07-03 22:15:52 +0200

    unit testing use cases

---
+++ tracker/tracker_domain/src/test/java/com/dvidal/tracker_domain/CalculateMealNutrientsTest.kt
@@ -0,0 +1,70 @@
+package com.dvidal.tracker_domain
+
+import com.dvidal.core.domain.model.ActivityLevel
+import com.dvidal.core.domain.model.Gender
+import com.dvidal.core.domain.model.GoalType
+import com.dvidal.core.domain.model.UserInfo
+import com.dvidal.core.domain.preferences.Preferences
+import com.dvidal.tracker_domain.model.MealType
+import com.dvidal.tracker_domain.model.TrackedFood
+import com.dvidal.tracker_domain.usecase.CalculateMealNutrients
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+import kotlin.random.Random
+
+class CalculateMealNutrientsTest {
+
+    private lateinit var calculateMealNutrients: CalculateMealNutrients
+
+    @Before
+    fun setUp() {
+        val preferences = mockk<Preferences>(relaxed = true)
+        every { preferences.loadUserInfo() } returns UserInfo(
+            gender = Gender.Male,
+            age = 20,
+            weight = 80f,
+            height = 180,
+            activityLevel = ActivityLevel.Medium,
+            goalType = GoalType.KeepWeight,
+            carbRatio = 0.4f,
+            proteinRatio = 0.3f,
+            fatRatio = 0.3f
+        )
+        calculateMealNutrients = CalculateMealNutrients(preferences)
+    }
+
+    @Test
+    fun `calories for breakfast properly calculated`() {
+        val trackedFoods = (1..30).map {
+            TrackedFood(
+                name = "trackedFoodName",
+                carbs = Random.nextInt(100),
+                protein = Random.nextInt(100),
+                fat = Random.nextInt(100),
+                mealType = MealType.fromString(
+                    listOf("breakfast", "lunch", "snack", "dinner").random()
+                ),
+                imageUrl = null,
+                amount = 100,
+                date = LocalDate.now(),
+                calories = Random.nextInt(2000)
+            )
+        }
+
+        val result = calculateMealNutrients.invoke(trackedFoods)
+
+        val breakfastCalories = result.mealNutrients.values
+            .filter { it.mealType is MealType.Breakfast }
+            .sumOf { it.calories }
+
+        val expectedCalories = trackedFoods
+            .filter { it.mealType is MealType.Breakfast }
+            .sumOf { it.calories }
+
+        assertThat(breakfastCalories).isEqualTo(expectedCalories)
+    }
+}
